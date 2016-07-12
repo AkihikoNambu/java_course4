f6110ce68166588a3b47625fc192686b7bad502f
Masa <cmasad743@gmail.com>
2016-07-12 17:21:05 +0900

    page19

---
+++ page19/Main.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+    Bicycle bicycle = new Bicycle();
+    bicycle.printData();
+    Car car = new Car();
+    car.setFuel(100);
+    car.printData();
+    System.out.println("ガソリン量: " + car.getFuel() + " (L)");
+  }
+}
