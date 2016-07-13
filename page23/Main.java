f45893b4520f5ece5fa3c4c7927bfa906f638a8c
Masa <cmasad743@gmail.com>
2016-07-13 11:04:54 +0900

    page23

--- page23/Main.java
+++ page23/Main.java
@@ -1,8 +1,8 @@
 class Main {
   public static void main(String[] args) {
-    Person person = new Person("Kate", "Jones", 27, 1.6, 50.0);
+    Person person1 = new Person("Kate", "Jones", 27, 1.6, 50.0);
 
-    Car car = new Car("車", person);
+    Car car = new Car("車", person1);
     System.out.println("【車の情報】");
     car.printData();
 
